7bf8dd500ac6f432ba7a4dfe546598fe8d1fe9c2
elaraby elaidy <elarabyelaidy@gmail.com>
2021-03-24 17:36:00 +0200

    etks7naa w 3menna

--- SoftwareEngineering/Foundation_Recap/recap_exercise_2/lib/problems.rb
+++ SoftwareEngineering/Foundation_Recap/recap_exercise_2/lib/problems.rb
@@ -26,6 +26,13 @@ end
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
+        new_hash = {}
+
+        self.each do |k, v|
+            new_hash[v] = k
+        end 
+
+        new_hash
 
     end
 end
@@ -34,7 +41,16 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
+        count = 0
 
+        (0...self.length).each do |idx_1|
+            (idx_1 + 1...self.length).each do |idx_2|
+                if self[idx_1] + self[idx_2] == num
+                    count += 1
+                end 
+            end 
+        end 
+        count 
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -51,6 +67,20 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        prc ||= Proc.new { |a, b| a <=> b }
 
+        sorted = false 
+
+        while !sorted 
+            sorted = true 
+            (0...self.length - 1).each do |i|
+                if prc.call(self[i], self[i + 1]) == 1 
+                    sorted = false 
+                    self[i], self[i + 1] = self[i + 1], self[i]
+                end
+            end 
+        end 
+
+        self 
     end
 end
