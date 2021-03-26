4d31b5ebcfcd554e09ec60329a6cebf7e2b14054
elaraby elaidy <elarabyelaidy@gmail.com>
2021-03-26 14:55:39 +0200

    third

--- SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
+++ SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
@@ -25,3 +25,20 @@ def no_consecutive_repeats(array)
     end 
   end 
 end 
+
+#char_indices
+#Write a method char_indices(str) that takes in a string as an arg. 
+#The method should return a hash containing characters as keys. 
+#The value associated with each key should be an array containing the indices where that character is found.
+
+def char_indices(str)
+  indices = Hash.new { |h, k| h[k] = [] }
+  str.each_char.with_index do |char, idx|
+    indices[char] << idx 
+  end 
+  indices
+end 
+
+## Examples
+char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
