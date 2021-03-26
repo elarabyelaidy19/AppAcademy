ef653271993b56fff2f8abe59555f3b663e0ae49
elaraby elaidy <elarabyelaidy@gmail.com>
2021-03-26 15:13:27 +0200

    fourth

--- SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
+++ SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
@@ -42,3 +42,30 @@ end
 ## Examples
 char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
 char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+
+
+#longest_streak
+#Write a method longest_streak(str) that accepts a string as an arg. 
+#The method should return the longest streak of consecutive characters in the string.
+#If there are any ties, return the streak that occurs later in the string.
+
+def longest_streak(str)
+  current = ""
+  longest = ""
+
+  (0...length).each do |i|
+    if str[i] == str[i - 1] || i == 0 
+      current += str[i]
+    else 
+      current = str[i]
+    end 
+
+    if current.length >= longest.length
+    longest = current
+    end 
+  end 
+
+  longest 
+
+end 
+
