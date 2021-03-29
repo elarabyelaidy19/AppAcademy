4b4a1493349850117114740af00ac6be135bb346
elaraby elaidy <elarabyelaidy@gmail.com>
2021-03-30 01:24:10 +0200

    backTOback

--- SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
+++ SoftwareEngineering/Foundation_Recap/recap_exercise_3/recap_exercise_3.rb
@@ -106,4 +106,26 @@ end
 
 
 
+# vigenere_cipher
+# A Caesar cipher takes a word and encrypts it by offsetting each letter in the word by a fixed number, called the key.
+# Given a key of 3, for example: a -> d, p -> s, and y -> b.
+# For example, if we encrypt "bananasinpajamas" with the key sequence 1, 2, 3, then the result would be "ccqbpdtkqqcmbodt":
+# Message:  b a n a n a s i n p a j a m a s
+# Keys:     1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1
+# Result:   c c q b p d t k q q c m b o d t
 
+# Write a method vigenere_cipher(message, keys) that accepts a string and a key-sequence as args, returning the encrypted message.
+# Assume that the message consists of only lowercase alphabetic characters.
+
+def vigenere_cipher(message, keys)
+  
+  alpha = ('a'..'z').to_a
+  new_str = ''
+
+  message.each_char.with_index do |char, idx|
+    old_pos = alpha.index(char)
+    new_pos = old_pos + keys[ idx % keys.length]
+    new_str += alpha[new_pos % alpha.length]
+  end 
+  new_str
+end 
