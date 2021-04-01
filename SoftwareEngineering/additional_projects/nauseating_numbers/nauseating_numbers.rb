76c1f64c3e7bd6cbbd2f71124857e6248f610397
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-02 01:06:13 +0200

    rampant_repeats

--- SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
+++ SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
@@ -48,3 +48,32 @@ p pair_product([3, 4, 6, 2, 5], 12) # true
 p pair_product([4, 2, 5, 7], 16)    # false
 p pair_product([8, 4, 9, 3], 8)     # false
 p pair_product([3], 12)             # false
+
+
+# rampant_repeats
+# Write a method rampant_repeats that accepts a string and a hash as arguments.
+# The method should return a new string where characters of the original string are repeated the number of times specified by the hash.
+# If a character does not exist as a key of the hash, then it should remain unchanged.
+
+
+def rampant_repeats(string, hash)
+  new_string = ' '
+
+  string.each_char do |char|
+    if hash.has_key?(char)
+      new_string += char * hash[char]
+    else 
+      new_string += char
+    end 
+  end 
+
+  new_string
+end 
+
+
+
+
+p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
---
+++ SoftwareEngineering/additional_projects/nauseating_numbers/tricks.md
@@ -0,0 +1,2 @@
+# tricks 
+ * hash.has_key?(key) ==> detect if the hash has a key == valuse
