946eb21ce4f3a0e34ccb7db0314e808b2dff74b9
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-02 01:19:52 +0200

    perfect_square

--- SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
+++ SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
@@ -77,3 +77,38 @@ p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
 p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
 p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
 p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+
+
+# perfect_square?
+# Write a method perfect_square? that accepts a number as an argument.
+# The method should return a boolean indicating whether or not the argument is a perfect square.
+# A perfect square is a number that is the product of some number multiplied by itself.
+# For example, since 64 = 8 * 8 and 144 = 12 * 12, 64 and 144 are perfect squares; 35 is not a perfect square.
+
+
+def perfect_square?(num)
+  
+  (1..num).each do |product|
+    if (product * product) == num  
+      return true 
+    end 
+  end
+  false 
+end  
+
+
+
+
+
+
+
+
+p perfect_square(1)     # true
+p perfect_square(4)     # true
+p perfect_square(64)    # true
+p perfect_square(100)   # true
+p perfect_square(169)   # true
+p perfect_square(2)     # false
+p perfect_square(40)    # false
+p perfect_square(32)    # false
+p perfect_square(50)    # false
