b264378cc06229b941b7e095203b64c608814b92
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-01 02:13:03 +0200

    part_2 finish

--- SoftwareEngineering/additional_projects/rspec_exercise_4/lib/part_2.rb
+++ SoftwareEngineering/additional_projects/rspec_exercise_4/lib/part_2.rb
@@ -10,3 +10,19 @@ end
 def aliquot_sum(num)
   proper_factors(num).sum
 end 
+
+def perfect_number?(num)
+  aliquot_sum(num) == num 
+end 
+
+def ideal_numbers(n)
+  array = []
+
+  i = 1
+  while array.length < n
+    array << i if perfect_number(i)
+    i += 1
+  end 
+
+  array
+end 
