68260e7661af12787f9975a14b76726f68b04e63
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-02 02:33:32 +0200

    2 problems added

--- SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
+++ SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
@@ -126,10 +126,7 @@ def anti_prime?(num)
 end 
 
 
-  
 
-
-end 
 p anti_prime?(24)   # true
 p anti_prime?(36)   # true
 p anti_prime?(48)   # true
@@ -141,5 +138,68 @@ p anti_prime?(100)  # false
 p anti_prime?(136)  # false
 p anti_prime?(1024) # false
 
+
+# matrix_addition
+# Let a 2-dimensional array be known as a "matrix".
+# Write a method matrix_addition that accepts two matrices as arguments. 
+# The two matrices are guaranteed to have the same "height" and "width". 
+# The method should return a new matrix representing the sum of the two arguments.
+# To add matrices, we simply add the values at the same positions:
+
+def matrix_addition(m1, m2)
+  height = m1.length
+  width = m1[0].length
+  result = Array.new(height) { [0] * width }
+
+  (0...height).each do |row|
+    (0...width).each do |col|
+      result[row][col] = m1[row][col] + m2[row][col]
+    end 
+  end 
+
+  result
+end 
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+
+
+# ==========================================================================================
+
+
+# mutual_factors
+# Write a method mutual_factors that accepts any amount of numbers as arguments.
+# The method should return an array containing all of the common divisors shared among the arguments.
+# For example, the common divisors of 50 and 30 are 1, 2, 5, 10.
+# You can assume that all of the arguments are positive integers.
+
+def factors(num)
+  (1..num).select { |i| num % i == 0 }
+end 
+
+def mutual_factors(*nums)
+  nums.map {|n| factors(n) }.inject(:&)
+end 
+
+
+
+p mutual_factors(50, 30)            # [1, 2, 5, 10]
+p mutual_factors(50, 30, 45, 105)   # [1, 5]
+p mutual_factors(8, 4)              # [1, 2, 4]
+p mutual_factors(8, 4, 10)          # [1, 2]
+p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+p mutual_factors(12, 24, 64)        # [1, 2, 4]
+p mutual_factors(22, 44)            # [1, 2, 11, 22]
+p mutual_factors(22, 44, 11)        # [1, 11]
+p mutual_factors(7)                 # [1, 7]
+p mutual_factors(7, 9)              # [1]
 
 
