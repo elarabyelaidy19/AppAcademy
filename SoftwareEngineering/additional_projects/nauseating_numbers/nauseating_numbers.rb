bc9bb7db176ba169b5fd1094797dd7432e662661
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-02 02:42:11 +0200

    tribonacci_number

--- SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
+++ SoftwareEngineering/additional_projects/nauseating_numbers/nauseating_numbers.rb
@@ -203,3 +203,33 @@ p mutual_factors(7)                 # [1, 7]
 p mutual_factors(7, 9)              # [1]
 
 
+# tribonacci_number
+# The tribonacci sequence is similar to that of Fibonacci.
+# The first three numbers of the tribonacci sequence are 1, 1, and 2.
+# To generate the next number of the sequence, we take the sum of the previous three numbers.
+# The first six numbers of tribonacci sequence are:
+# 1, 1, 2, 4, 7, 13, ... and so on
+# Write a method tribonacci_number that accepts
+# a number argument, n, and returns the n-th number of the tribonacci sequence.
+
+def tribonacci_number(n)
+  sequence = [1, 1, 2]
+
+  while sequence.length <= n
+    sequence << sequence[-3] + sequence[-2] + sequence[-1]
+  end 
+  sequence[n - 1]
+end 
+  
+
+
+
+
+p tribonacci_number(1)  # 1
+p tribonacci_number(2)  # 1
+p tribonacci_number(3)  # 2
+p tribonacci_number(4)  # 4
+p tribonacci_number(5)  # 7
+p tribonacci_number(6)  # 13
+p tribonacci_number(7)  # 24
+p tribonacci_number(11) # 274
