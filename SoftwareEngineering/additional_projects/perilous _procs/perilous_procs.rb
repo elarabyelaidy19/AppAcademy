ef107a9d5fe854b3cfbd73b816cf46b935c522e2
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-03 12:20:27 +0200

    stay with procs

--- SoftwareEngineering/additional_projects/perilous _procs/perilous_procs.rb
+++ SoftwareEngineering/additional_projects/perilous _procs/perilous_procs.rb
@@ -64,3 +64,59 @@ def at_least?(array, n, &prc)
   count >= n 
 end 
 
+
+
+# every?
+# Write a method every? that accepts an array and a block as arguments. 
+# The method should return a boolean indicating whether or not all elements of the array return true when given to the block.
+# Solve this using Array#each.
+
+def every?(array, &prc)
+  array.each do |ele|
+    return false if !prc.call(ele)
+  end 
+  true 
+end 
+
+
+# at_most?
+# Write a method at_most? that accepts an array, a number (n), and a block as arguments.
+# The method should return a boolean indicating whether no more than n elements of the array return true when given to the block.
+# Solve this using Array#each. 
+
+def at_most(array, n, &prc)
+  count = 0
+  array.each do |ele| 
+    count += 1 if prc.call(ele)
+  end 
+  count < n 
+end 
+
+
+# first_index
+# Write a method first_index that accepts an array and a block as arguments. 
+# The method should return the index of the first element of the array that returns true when given to the block. 
+# If no element of returns true, then the method should return nil. 
+# Solve this using Array#each.
+
+def first_index(array, &prc)
+  array.each do |ele, i| 
+    return i if prc.call(ele)
+  end 
+  nil
+end 
+
+
+# Phase 2: The proc thickens.
+# xnor_select
+# Write a method xnor_select that accepts an array and two procs as arguments.
+# The method should return a new array containing elements of the original array that either return true for 
+# both procs or return false for both procs.
+
+def xnor_select(array, prc_1, prc_2)
+  selected = []
+  array.each do |ele|
+    selected << ele if prc_1.call(ele) == prc.call(ele)
+  end 
+  selected
+end 
