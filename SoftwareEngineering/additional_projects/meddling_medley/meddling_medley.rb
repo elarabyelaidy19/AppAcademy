a5d2abafa4d68f5c33602e64c83b0023e12ea287
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-04 18:48:34 +0200

    unsolve the solved

--- SoftwareEngineering/additional_projects/meddling_medley/meddling_medley.rb
+++ SoftwareEngineering/additional_projects/meddling_medley/meddling_medley.rb
@@ -154,3 +154,66 @@ def uncompress(string)
 
   uncompressed
 end 
+
+
+# Phase 2: More difficult, maybe?
+# conjunct_select
+# Write a method conjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed into all of the given procs. 
+
+# is_positive = Proc.new { |n| n > 0 }
+# is_odd = Proc.new { |n| n.odd? }
+# less_than_ten = Proc.new { |n| n < 10 }
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
+def conjunct_select(array, *prcs)
+  prcs.select { |ele| prcs.all? { |prc| prc.call(ele) } }
+end 
+
+def conjunct_select(array, *prcs) 
+  prcs.inject(array) {|acc, prc| acc.select(&prc) } 
+end 
+
+
+
+# convert_pig_latin
+# Write a method convert_pig_latin that accepts a sentence as an argument.
+# The method should translate the sentence according to the following rules
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+# if the word begins with a vowel, simply add 'yay' to the end of the word (example: 'eat'->'eatyay')
+# if the word begins with a non-vowel, move all letters that come before the word's first vowel to the end of the word and add 'ay' (example: 'trash'->'ashtray')
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+
+
+def convert_pig_latin(sentence)
+  words = sentence.split(' ') 
+  new_words = words.map do |word|
+    word == word.capitalize ? new_word.capitalize : new_word
+    new_word = word.length > 2 ? convert_word(word) : word 
+  end 
+  new_words.join(' ')
+end 
+
+def convert_word(word)
+  vowels = 'AEIOUaeiou'
+  return word += 'yay' if vowel.include?(word[0]) 
+  word.each_char.with_index do |char, i|
+    if vowels.include?(char) 
+      return word[i..-1] + word[0...i] + 'ay' 
+    end 
+  end 
+end 
+
+
+
+  
+
+  
