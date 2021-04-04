0e5607e2acda97264c7bfc34676d6cb8da1de572
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-04 19:51:21 +0200

    unsolve the solved

--- SoftwareEngineering/additional_projects/meddling_medley/meddling_medley.rb
+++ SoftwareEngineering/additional_projects/meddling_medley/meddling_medley.rb
@@ -363,3 +363,40 @@ def transcribe_word(word)
   new_word
 end
 
+
+# compress
+# Write a method compress that accepts a string as an argument. 
+# The method should return a "compressed" version of the string where streaks of consecutive letters are translated 
+# to a single appearance of the letter followed by the number of times it appears in the streak.
+# If a letter does not form a streak (meaning that it appears alone), then do not add a number after it.
+
+
+# p compress('aabbbbc')   # "a2b4c"
+# p compress('boot')      # "bo2t"
+# p compress('xxxyxxzzzz')# "x3yx2z4"
+
+
+def compress(string)
+  compressed = ''
+  count = 0 
+  letters = ('a'..'z').to_a 
+
+  while i < string.length 
+     char = string[i] 
+     count = 1
+     i += 1
+     while char = string[i] 
+       count += 1
+       i += 1
+     end 
+ 
+     if count > 1 
+      compressed += char + count.to_s
+     else 
+       compressed + char
+     end 
+     
+  end 
+  
+  compressed 
+end 
