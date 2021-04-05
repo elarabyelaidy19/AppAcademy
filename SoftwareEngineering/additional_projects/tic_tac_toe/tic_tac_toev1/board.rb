707543771637c914780e44f8cf2dd50c70a58bd8
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-05 20:49:21 +0200

    play_tictactoe

--- SoftwareEngineering/additional_projects/tic_tac_toe/tic_tac_toev1/board.rb
+++ SoftwareEngineering/additional_projects/tic_tac_toe/tic_tac_toev1/board.rb
@@ -29,6 +29,49 @@ class Board
     raise "error" if !valid?(pos) || !empty?(pos)
     self[pos] = mark
   end 
+
+  def print 
+    @grid.each do |row|
+      puts row.join('')
+    end 
+  end 
+
+  def win_row?(mark) 
+    @grid.any? { |row| row.all?(mark) }
+  end 
+
+  def win_col?(mark) 
+    @grid.any? { |col| col.all?(mark) }
+  end 
+
+  def win_diagonal?(mark) 
+    left_to_right = (0...@grid.legth).all? do |i|
+      pos = [i, i] 
+      self[pos] = mark 
+    end 
+
+    right_to_left = (0...@grid.legth - 1).all? do |i|
+      row = i 
+      col = @grid.legth -1 - i 
+      pos = [row, col] 
+      self[pos] = mark 
+    end 
+
+    right_to_left || left_to_right 
+  end 
+
+  def win?(mark) 
+    win_diagonal?(mark) || win_col?(mark) || win_row(mark)
+  end 
+
+  def empty_positions?
+    
+
+
+
+
+
+
 end 
 
 
