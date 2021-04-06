9d86e7caa361985fc8a27c77288518ad49c3529d
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-07 01:57:11 +0200

    finish V1

---
+++ SoftwareEngineering/additional_projects/todo_board/todo_board.rb
@@ -0,0 +1,51 @@
+require './list.rb' 
+
+class TodoBoard 
+
+  def initialize(label) 
+    @list = List.new(label) 
+  end 
+
+  def get_command
+    print "/nEnter a command: "
+    cmd, *args = gets.chomp.splt(' ')
+
+    case cmd 
+      when 'mktodo'
+        @list.add_item(*args)
+      when 'up'
+        @list.up(*args.map(&:to_i))
+      when 'down'
+        @list.down(*args.map(&:to_i)) 
+      when 'swap' 
+        @list.swap(*args.map(&:to_i)) 
+      when 'sort'
+        @list.sort_by_date! 
+      when 'priority'
+        @list.print_priority
+      when 'print'
+        if args.empty?
+          @list.print 
+        else
+          list.print_full_item(args[0].to_i)
+        end 
+      when 'quit'
+        return false
+      else 
+        print ' unspecified command' 
+      end 
+
+      true 
+    end 
+  end 
+
+    def run
+      while true
+        return if !get_command
+      end 
+    end
+end 
+
+
+
+
