2cce7e670cb9d9b8af5a4ab0664fb9b58aa36a3a
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-07 00:07:06 +0200

    making todo as a todo

---
+++ SoftwareEngineering/additional_projects/todo_board/item.rb
@@ -0,0 +1,30 @@
+class Item
+  def self.valid_date?(date)
+       parts = date.split('-').map(&:to_i) 
+       year, month, day = parts 
+       return false if parts.length != 3 
+       return false if !(0..12).include?(month)
+       return false if !(0..31).include?(day)
+       true 
+  end 
+ 
+  attr_reader :deadline
+  attr_accessor :title, :description
+
+  def initialize(title, deadline, description)
+     @title = title
+      @deadline = deadline
+     @description = description
+     raise "invalid date" if !Item.valid_date?(deadline)
+  end 
+
+  def deadline=(deadline)
+     raise 'invalid date' if !Item.valid_date?(new_deadline)
+     @deadline = deadline
+  end 
+  
+end 
+
+
+
+
