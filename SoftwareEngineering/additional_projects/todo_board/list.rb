6fad61b094b5e5f13c5cbe8a74b275e6103802b3
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-07 01:37:29 +0200

    making todo as a todo

--- SoftwareEngineering/additional_projects/todo_board/list.rb
+++ SoftwareEngineering/additional_projects/todo_board/list.rb
@@ -1,6 +1,12 @@
 require './item.rb'
 
 class List
+  
+  #print style 
+  LINE_WIDTH = 42
+  INDEX_COL_WIDTH = 5 
+  ITEM_COL_WIDTH = 20 
+  DEADLINE_COL_WIDTH = 10
 
   attr_accessor :label
 
@@ -33,6 +39,59 @@ class List
     return nil if !valid_index?(index)
     @items[index]
   end 
+
+  def print
+    puts "-" * LINE_WIDTH
+    puts " " * 16 + self.label.upcase
+    puts "-" * LINE_WIDTH
+    puts "#{'Index'.ljust(INDEX_COL_WIDTH)} | #{'Item'.ljust(ITEM_COL_WIDTH)} | #{'Deadline'.ljust(DEADLINE_COL_WIDTH)}"
+    puts "-" * LINE_WIDTH
+    @items.each_with_index do |item, i|
+        puts "#{i.to_s.ljust(INDEX_COL_WIDTH)} | #{item.title.ljust(ITEM_COL_WIDTH)} | #{item.deadline.ljust(DEADLINE_COL_WIDTH)}"
+    end
+    puts "-" * LINE_WIDTH
+end
+
+ def print_full_item(index)
+    item = self[index]
+    return if item.nil?
+    puts "-" * LINE_WIDTH
+    puts "#{item.title.ljust(LINE_WIDTH/2)}#{item.deadline.rjust(LINE_WIDTH/2)}"
+    puts item.description
+    puts "-" * LINE_WIDTH
+ end
+
+  def print_priority
+    print_full_item(0)
+  end 
+
+  def up(index, amount = 1) 
+    return false if !valid_index?(index)
+    while amount > 0 && index != 0 
+      swap(index, index - 1)
+      index -= 1 
+      amount -= 1
+    end 
+    true 
+  end 
+  
+  def down(index, amount = 1)
+    return false if !valid_index?(index) 
+    while amount > 0 && index != size - 1 
+      swap(index, index + 1)
+      index += 1 
+      amount -= 1 
+    end 
+    true 
+  end 
+
+  def sort_by_date!
+    @items.sort_by! {|item| item.deadline }
+  end 
+
+
+
+
 
 end 
 
