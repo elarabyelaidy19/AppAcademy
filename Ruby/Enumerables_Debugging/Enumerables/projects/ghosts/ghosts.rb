68359674013c21c2afa001b061a987f784436773
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-09 04:07:00 +0200

    ghosts level

--- Ruby/Enumerables_Debugging/Enumerables/projects/ghosts/ghosts.rb
+++ Ruby/Enumerables_Debugging/Enumerables/projects/ghosts/ghosts.rb
@@ -13,6 +13,17 @@ class GhostGame
     @losses = Hash.new { |losses, player| losses[player] = 0 }
   end 
 
+
+  def add_letter(letter)  
+    fragment << letter
+  end  
+
+  def valid_play?(letter) 
+    return false unless ALPHABET.include?(letter)
+    potential_fragment = fragment + letter 
+    dictionary.any? { |word| word.start_with?(potential_fragment) }  
+  end 
+
   def current_player 
     @players.first 
   end 
@@ -21,5 +32,40 @@ class GhostGame
     players.rotate! 
     players.rotate! until losses[current_player] < MAX_LOSS_COUNT 
   end 
+
+  def is_word?(fragment) 
+    dictionary.include?(fragment) 
+  end 
+
+  def round_over?
+    is_word?(fragment) 
+  end 
+
+  def previous_player
+    (players.count - 1).downto(0).each do |idx| 
+      player = players[idx] 
+
+      return player if losses[player] < MAX_LOSS_COUNT 
+    end 
+  end  
+
+  def remaining_players 
+    losses.count { |k, v| v < MAX_LOSS_COUNT } 
+  end 
+
+  def winner 
+    (player, k) = losses.find { |k, losses| losses < MAX_LOSS_COUNT } 
+    player
+  end 
+
+  def record(player) 
+
+
+
+
+
+
+
+
 
 
