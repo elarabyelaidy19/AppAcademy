24857b1273f7ac00cf0bb120061f0d8c07abdf70
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-09 03:48:41 +0200

    ghosts level

--- Ruby/Enumerables_Debugging/Enumerables/projects/ghosts/ghosts.rb
+++ Ruby/Enumerables_Debugging/Enumerables/projects/ghosts/ghosts.rb
@@ -9,4 +9,17 @@ class GhostGame
   def initialize(*players) 
     words = File.readlines("dictionary.txr").map(&:chomp) 
     @dictionary = Set.new(words) 
-     
+    @players = players 
+    @losses = Hash.new { |losses, player| losses[player] = 0 }
+  end 
+
+  def current_player 
+    @players.first 
+  end 
+
+  def next_player 
+    players.rotate! 
+    players.rotate! until losses[current_player] < MAX_LOSS_COUNT 
+  end 
+
+
