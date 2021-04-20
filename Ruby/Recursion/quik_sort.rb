c6bf466ae67c38d3ba9988c0e5e14dec4a5ceb2e
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-20 21:16:40 +0200

    quik_sort implementation

--- Ruby/Recursion/quik_sort.rb
+++ Ruby/Recursion/quik_sort.rb
@@ -6,6 +6,8 @@ def quik_sort(arr)
   left_side = arr[1..-1].select { |ele| ele < arr.first } 
   right_side = arr[1..-1].select { |ele| ele >= arr.first }
   quik_sort(left_side) + pivot + quik_sort(right_side)
+
 end 
 
 p quik_sort([3, 8, 8, 9, 2, 1, 6])
+
