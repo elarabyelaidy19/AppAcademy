26febb70812f6b62c16079ad4b856ce3ed91a608
elaraby elaidy <elarabyelaidy@gmail.com>
2021-05-01 15:42:39 +0200

    stack array implemintation

---
+++ Ruby/data_structures/stack.rb
@@ -0,0 +1,39 @@
+class Stack 
+
+
+  attr_accessor :ivar
+
+  def initialize 
+    @ivar = [] 
+  end 
+
+  def push(ele)
+    ivar.push(ele)
+    ele
+  end 
+
+  def pop 
+    ivar.pop 
+  end 
+
+  def peek 
+    ivar.last  
+  end 
+
+end  
+
+stack = Stack.new
+
+p stack.push(2)
+p stack.push(3) 
+p stack.push(4) 
+p stack.push(4) 
+puts
+
+p stack.pop
+p stack.pop
+
+
+
+
+
