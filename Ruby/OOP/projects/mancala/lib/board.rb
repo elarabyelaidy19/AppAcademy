0bc7b48fdba8ef1687d17287f7e54188b41973b2
elaraby elaidy <elarabyelaidy@gmail.com>
2021-05-04 22:10:39 +0200

    next turn added

--- Ruby/OOP/projects/mancala/lib/board.rb
+++ Ruby/OOP/projects/mancala/lib/board.rb
@@ -48,8 +48,17 @@ class Board
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
-
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx 
+    if ending_cup_idx == 6 || ending_cup_idx == 13  
+      #ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1 
+      # ended on empty cup -- switches players' turns
+      :switch 
+    else  
+       # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end 
   end
 
   def render
