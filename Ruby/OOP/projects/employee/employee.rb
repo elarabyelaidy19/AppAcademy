5b8470e459e82e9bae55ddbf145e34b54377b6cd
elaraby elaidy <elarabyelaidy@gmail.com>
2021-05-15 15:56:55 +0200

    last

--- Ruby/OOP/projects/employee/employee.rb
+++ Ruby/OOP/projects/employee/employee.rb
@@ -8,28 +8,27 @@ class Employee
     @salary = salary
     self.boss = boss
   end  
-
-  def boss =(boss) 
-    @boss = boss 
-    boss.add_employee(self) unless boss.nil? 
-    boss 
-  end 
+  
+  def boss=(boss)
+    @boss = boss
+    boss.add_employee(self) unless boss.nil?
+  end
 
   def bonus(multiplier) 
-   @salary * multiplier
+   self.salary * multiplier
   end 
 end 
 
 class Manager < Employee 
-  attr_reader :employee 
+  attr_reader :employees 
 
   def initialize(name, title, salary, boss = nil) 
     super(name, title, salary, boss) 
-    @employee = [] 
+    @employees = [] 
   end 
 
   def add_employee(subordinate) 
-    employee << subordinate 
+    @employees << subordinate 
     subordinate
   end 
 
@@ -37,10 +36,12 @@ class Manager < Employee
     self.total_subsalary * multiplier
   end 
 
+  protected
+
   def total_subsalary
     total_subsalary = 0 
-    self.employee.each do |employee| 
-      if employee.is_a(Manager) 
+    self.employees.each do |employee| 
+      if employee.is_a?(Manager) 
         total_subsalary += employee.salary + employee.total_subsalary 
       else  
         total_subsalary += employee.salary 
@@ -49,5 +50,4 @@ class Manager < Employee
     total_subsalary
   end 
 end 
-
 
