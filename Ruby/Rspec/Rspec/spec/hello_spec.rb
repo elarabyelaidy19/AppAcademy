d097c2709a5306b92d04e5c2d6daf298bcc16bde
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-04 23:56:17 +0200

    rspec

--- Ruby/Rspec/Rspec/spec/hello_spec.rb
+++ Ruby/Rspec/Rspec/spec/hello_spec.rb
@@ -10,11 +10,18 @@ end
 
 
 describe Integer do 
-  describe 'to_s' do  
+  describe '#to_s' do  
     it 'return string to integer' do  
       expect(5.to_s).to eq('5') 
     end 
   end 
 end 
+
+describe '#sqrt' do 
+  it 'throw an error' do 
+    expect { sqrt(-3) }.to raise_error(ArgumentError) 
+  end 
+end 
+
 
 
