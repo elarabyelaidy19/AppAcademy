286715e6437e909ff21defd0847ad277ffba3ebb
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-09 02:09:55 +0200

    uniqe

--- Ruby/Rspec/TDD/lib/array.rb
+++ Ruby/Rspec/TDD/lib/array.rb
@@ -8,3 +8,19 @@ def uniq(array)
   uniq_array
 end 
 
+
+def two_sum(array) 
+  pairs = [] 
+
+  len = array.length - 1 
+
+  (0..len).each do |p1| 
+    (p1 + 1..len).each do |p2| 
+      pairs << [p1, p2] if array[p1] + array[p2] == 0
+    end 
+  end 
+
+  pairs 
+end 
+
+p two_sum([-5, -3, 1, 3])
