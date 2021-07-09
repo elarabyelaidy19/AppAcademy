20ac7853573340fe97b171efdc5c3ab5eece886a
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-10 00:15:43 +0200

    best time to buy stocks

--- Ruby/Rspec/TDD/lib/array.rb
+++ Ruby/Rspec/TDD/lib/array.rb
@@ -40,3 +40,26 @@ def transpose(rows)
 end
 
 
+
+def pick_stocks(prices) 
+  best_profit = 0 
+  best_pair = nil 
+  
+  prices.each do |buy_date| 
+    prices.each do |sell_date| 
+
+      next if sell_date < buy_date 
+
+      profit = prices[sell_date] - prices[buy_date] 
+
+      if profit > best_profit
+        best_pair, best_profit = [buy_date, sell_date], profit
+      end 
+    end 
+  end 
+
+  best_pair
+end 
+
+
+
