6d779137e4368a461ecdceb92b7e519b15cb4bb0
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-10 02:01:49 +0200

    tower of hanoi

--- Ruby/Rspec/TDD/spec/hanoi_spec.rb
+++ Ruby/Rspec/TDD/spec/hanoi_spec.rb
@@ -15,4 +15,26 @@ describe TowersOfHanoiGame do
     end
   end
 
-  
+
+  describe "#move" do
+    it "allows moving to a blank space" do
+      expect { towers.move(0, 1) }.not_to raise_error
+    end
+    
+    it "allows moving onto a larger disk" do
+      towers.move(0, 2)
+      towers.move(0, 1)
+      expect { towers.move(2, 1) }.not_to raise_error
+    end
+
+    it "does not allow moving from an empty stack" do
+      expect { towers.move(1, 2) }
+      .to raise_error("cannot move from empty stack")
+    end
+
+    it "does not allow moving onto a smaller disk" do
+      towers.move(0, 1)
+      expect { towers.move(0, 1) }
+      .to raise_error("cannot move onto smaller disk")
+    end
+  end
