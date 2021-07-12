fe255d0995f8ba420cb09363622f169e7d562a36
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-12 03:24:22 +0200

    BigO

--- Ruby/BigO/octupus_problrms.rb
+++ Ruby/BigO/octupus_problrms.rb
@@ -11,3 +11,47 @@ def quadratic_biggest_fish(fishes)
   return fish1 if max_length
 end 
 
+# -===========================================================
+
+class Array
+  #this should look familiar
+  def merge_sort(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    return self if count <= 1
+
+    midpoint = count / 2
+    sorted_left = self.take(midpoint).merge_sort(&prc)
+    sorted_right = self.drop(midpoint).merge_sort(&prc)
+
+    Array.merge(sorted_left, sorted_right, &prc)
+  end
+
+  private
+  def self.merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        merged << left.shift
+      when 0
+        merged << left.shift
+      when 1
+        merged << right.shift
+      end
+    end
+
+    merged.concat(left)
+    merged.concat(right)
+
+    merged
+  end
+end
+
+def nlogn_biggest_fish(fishes)
+  # sort the array longest to shortest
+  prc = Proc.new { |x, y| y.length <=> x.length }
+  #return the first element
+  fishes.merge_sort(&prc)[0]
+end
