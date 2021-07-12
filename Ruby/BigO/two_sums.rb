d51d8dcf8d6e4e342b2b9dbdce0aa0b9639b2c98
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-13 00:45:43 +0200

    two sums

--- Ruby/BigO/two_sums.rb
+++ Ruby/BigO/two_sums.rb
@@ -1,3 +1,5 @@
+# quadratic time 
+# constant time
 def bad_two_sum?(arr, target_sum) 
   (0..arr.length - 1).each do |i| 
     (i+1..arr.length - 1).each do |j|
@@ -8,4 +10,24 @@ def bad_two_sum?(arr, target_sum)
 end 
 
 
-p bad_two_sum?([3, 4, 2, 5], 5) 
+p bad_two_sum?([3, 4, 2, 5, 12], 17) 
+
+
+# linearithmic time
+# linear time 
+def okay_two_sum_a?(arr, target_sum) 
+  arr = arr.sort 
+  i, j = 0, arr.length - 1 
+
+  while j > i 
+    case (arr[i] + arr[j]) <=> target_sum 
+      when 0 
+        return true 
+      when -1 
+        i += 1 
+      when 1 
+        j -= 1 
+    end 
+  end 
+  false 
+end 
