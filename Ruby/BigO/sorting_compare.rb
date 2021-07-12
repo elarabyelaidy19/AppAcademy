56460ed55b4bf144577048df7681b3ae48c905ba
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-12 02:25:45 +0200

    BigO

---
+++ Ruby/BigO/sorting_compare.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'io/console'
+require 'benchmark'
+
+class SortingDemo
+  # Bubble Sort: O(n^2)
+  def self.bubble_sort!(array)
+    len = array.length - 1
+    sorted = false
+
+    until sorted
+      sorted = true
+      (0...len).each do |idx|
+        if array[idx] > array[idx + 1]
+          array[idx], array[idx + 1] = array[idx + 1], array[idx]
+          sorted = false
+        end
+      end
+    end
+
+    array
+  end
