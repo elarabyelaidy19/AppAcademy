f2cd2e9c4d15995ffe40b209dda454b2354d470e
elaraby elaidy <elarabyelaidy@gmail.com>
2021-07-14 02:29:17 +0200

    sql

---
+++ sql/sql_fundemental/my_database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE countries ( 
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255) NOT NULL,
+  population INTEGER NOT NULL 
+); 
+
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    country_id INTEGER NOT NULL,
+
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+);
+
