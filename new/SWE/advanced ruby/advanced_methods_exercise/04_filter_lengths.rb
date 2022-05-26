4dff4b58cb0c90ced8e82d54aa51b7b3f86e7e1b
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-05-26 16:59:12 +0200

    advanced methods

--- new/SWE/advanced ruby/advanced_methods_exercise/04_filter_lengths.rb
+++ new/SWE/advanced ruby/advanced_methods_exercise/04_filter_lengths.rb
@@ -3,7 +3,9 @@
 # that have at least the given length. The length argument should be optional; if no length
 # is passed in, then 5 should be used as the length.
 
-
+def filter_lengths(arr, le=5) 
+    arr.select {|ele| ele.length >= le}    
+end 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
 p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]
