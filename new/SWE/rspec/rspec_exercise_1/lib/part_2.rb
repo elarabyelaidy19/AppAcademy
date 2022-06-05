56a13b5214c0a8488605a270363a2998c2734962
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-06 00:44:19 +0200

    rspec

--- new/SWE/rspec/rspec_exercise_1/lib/part_2.rb
+++ new/SWE/rspec/rspec_exercise_1/lib/part_2.rb
@@ -1,1 +1,28 @@
-def hipsterfy
+def hipsterfy(str) 
+    str.reverse.sub(/[aioue]/, "").reverse
+end  
+
+def vowel_counts(str) 
+    hash = Hash.new(0)
+    vowels = "aioue" 
+    str.chars.each do |c| 
+        hash[c.downcase] += 1 if vowels.include?(c.downcase)
+    end  
+    hash
+end 
+
+
+def caesar_cipher(str, n) 
+    alph =  ("a".."z").to_a 
+    res = ""
+    str.chars.each do |c| 
+        if(alph.include?(c))
+            old_index = alph.index(c) 
+            new_index = old_index + n 
+            res += alph[new_index % 26] 
+        else  
+            res += c  
+        end  
+    end  
+    res
+end 
