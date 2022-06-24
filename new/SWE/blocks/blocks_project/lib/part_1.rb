73886029731b2fbb8e1af9ad73281baae6cc9028
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-25 01:54:51 +0200

    blocks

---
+++ new/SWE/blocks/blocks_project/lib/part_1.rb
@@ -0,0 +1,11 @@
+def select_even_nums(arr) 
+    arr.select { |n| n.even? }
+end  
+
+def reject_puppies(arr) 
+    arr.reject { |dog| dog["age"] <= 2 }
+end  
+
+def count_positive_subarrays(arr) 
+
+end
