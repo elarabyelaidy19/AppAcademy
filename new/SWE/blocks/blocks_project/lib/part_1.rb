a53e98fa5e1087a0be2991c25fcc2b0b116c45a8
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-25 02:06:38 +0200

    blocks part 1 complete

--- new/SWE/blocks/blocks_project/lib/part_1.rb
+++ new/SWE/blocks/blocks_project/lib/part_1.rb
@@ -7,5 +7,24 @@ def reject_puppies(arr)
 end  
 
 def count_positive_subarrays(arr) 
+    arr.count { |arr| arr.sum.positive? }
+end 
 
-end
+
+def aba_translate(str) 
+    vowel = "aioue" 
+    res = "" 
+    str.each_char do |c| 
+        if vowel.include?(c) 
+            res += "#{c}b#{c}" 
+        else 
+            res += c  
+        end 
+    end 
+    res
+end 
+
+
+def aba_array(arr) 
+    arr.map { |ele| aba_translate(ele) }
+end 
