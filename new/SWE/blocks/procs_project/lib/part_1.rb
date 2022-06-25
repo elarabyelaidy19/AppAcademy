f40df182118762bcad8e1a49d41c2b0b8b0586a6
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-25 03:26:41 +0200

    proc part 1 complete

--- new/SWE/blocks/procs_project/lib/part_1.rb
+++ new/SWE/blocks/procs_project/lib/part_1.rb
@@ -16,5 +16,33 @@ def my_select(arr, &proc)
 end 
 
 
+def my_count(arr, &proc) 
+    count = 0 
+    arr.each do |ele| 
+        count += 1 if proc.call(ele) 
+    end 
+    count
+end 
 
+
+def my_any?(arr, &proc)
+    arr.each do |ele| 
+        return true if proc.call(ele) 
+    end 
+    false
+end 
+
+def my_all?(arr, &proc) 
+    arr.each do |ele| 
+        return false if !proc.call(ele) 
+    end 
+    true
+end 
+
+def my_none?(arr, &proc) 
+    arr.each do |ele| 
+        return false if proc.call(ele) 
+    end 
+    true
+end 
 
