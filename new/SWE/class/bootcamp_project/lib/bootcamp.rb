e28435a5fd1ec3334092cb43dee76dc6d1c8f34c
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-06-26 23:44:05 +0200

    bootcamp class

--- new/SWE/class/bootcamp_project/lib/bootcamp.rb
+++ new/SWE/class/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,59 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity) 
+        @name = name 
+        @slogan = slogan 
+        @student_capacity = student_capacity 
+        @teachers = Array.new()
+        @students =  Array.new()
+        @grades = Hash.new { |h, k| h[k] = [] }
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def teachers 
+        @teachers 
+    end 
+
+    def students 
+        @students
+    end 
+
+    def slogan 
+        @slogan
+    end
+
+    def hire(teacher) 
+        teachers << teacher
+    end 
+
+    def enroll(student) 
+        if students.length < @student_capacity 
+            students << student 
+            true 
+        else 
+            false 
+        end 
+    end  
+
+    def enrolled?(student) 
+        @students.include?(student)
+    end 
+
+    def student_to_teacher_ratio 
+        (@students.length / @teachers.length).to_f
+    end 
+
+    def add_grade(student, grade) 
+        if(enrolled?(student))
+            @grades[student] << grade 
+            true 
+        else 
+            false
+        end 
+    end 
+
+    def num_grades()
+
 end
