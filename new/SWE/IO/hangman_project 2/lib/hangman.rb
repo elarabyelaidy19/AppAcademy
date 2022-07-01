2ecef9b9cd8f8eb2ada244e3e283d62347501ce9
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-01 07:08:54 +0200

    guess word

--- new/SWE/IO/hangman_project 2/lib/hangman.rb
+++ new/SWE/IO/hangman_project 2/lib/hangman.rb
@@ -1,4 +1,50 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+
+  def self.random_word 
+    random = DICTIONARY.sample
+  end 
+
+  def initialize 
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_") 
+    @attempted_chars = [] 
+    @remaining_incorrect_guesses = 5
+  end 
+
+  def guess_word 
+    @guess_word 
+  end 
+
+  def attempted_chars 
+    @attempted_chars
+  end 
+
+  def remaining_incorrect_guesses 
+    @remaining_incorrect_guesses
+  end 
+
+  def already_attempted?(char)
+    attempted_chars.include?(char)
+  end 
+
+  def get_matching_indices(char) 
+    arr = [] 
+    @secret_word.each_char.with_index do |c, i| 
+      arr << i if c == char  
+    end 
+    arr
+  end 
+
+  def fill_indices(char, arr) 
+    @guess_word.each_index do |i| 
+      if arr.include?(i) 
+        @guess_word[i] = char 
+      end
+    end 
+    @guess_word 
+  end  
+
+
 end
