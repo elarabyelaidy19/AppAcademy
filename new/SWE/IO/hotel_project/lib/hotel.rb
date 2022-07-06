515b84707118e0c02d5e0e5e5fb6e3c07da4a5a4
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-06 23:25:03 +0200

    model hotel

--- new/SWE/IO/hotel_project/lib/hotel.rb
+++ new/SWE/IO/hotel_project/lib/hotel.rb
@@ -1,5 +1,50 @@
 require_relative "room"
 
-class Hotel
-  
+class Hotel  
+    
+    
+    def initialize(name, hash) 
+        @rooms = {} 
+        @name = name 
+        
+        hash.each do |k, v| 
+            @rooms[k] = Room.new(v) 
+        end 
+    end 
+
+    def name 
+        @name.split(" ").map { |w| w.capitalize! }.join(" ")
+    end  
+
+    def rooms 
+        @rooms
+    end 
+
+    def room_exists?(room) 
+        @rooms.has_key?(room)
+    end  
+
+    def check_in(person, room) 
+        if self.room_exists?(room) 
+            if @rooms[room].add_occupant(person) 
+                p 'check in successful' 
+            else 
+                p 'sorry, room is full'
+            end 
+        else 
+            p 'sorry, room does not exist'
+        end  
+    end  
+
+    def has_vacancy?  
+        @rooms.values.any? { |room| room.available_space > 0 }
+    end 
+
+    def list_rooms 
+        
+        @rooms.each do |k, v| 
+            puts "#{k}:#{v.available_space}" 
+        end 
+        
+    end 
 end
--- new/SWE/IO/hotel_project/lib/room.rb
+++ new/SWE/IO/hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
+
+  attr_reader :capacity, :occupants 
   
+  def initialize(capacity) 
+    @capacity = capacity 
+    @occupants = [] 
+  end 
+
+  def full? 
+    @occupants.length >= @capacity
+  end 
+
+  def available_space 
+    @capacity - @occupants.length
+  end 
+
+  def add_occupant(person) 
+    if full? 
+       return false 
+    else 
+        @occupants << person 
+        return true 
+    end 
+  end 
+
 end
