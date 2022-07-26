ce589b2127631c64f91e9680e0bd6f0cd93677bd
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-27 00:15:05 +0200

    build startup

--- new/SWE/oop/startup_project/lib/startup.rb
+++ new/SWE/oop/startup_project/lib/startup.rb
@@ -31,6 +31,39 @@ class Startup
     end 
 
     def pay_employee(employee)
-        
+        employee_salary = @salaries[employee.title] 
+        if @funding > employee_salary  
+            employee.pay(employee_salary) 
+            @funding -= employee_salary 
+        else 
+            raise "no more funding" 
+        end 
+    end 
+
+    def payday
+        @employees.each { |employee| self.pay_employee(employee) }
+    end 
+
+    def average_salary 
+        salaries = 0
+        @employees.each do |emp| 
+            salaries += @salaries[emp.title] if @salaries.has_key?(emp.title)  
+        end 
+        salaries / @employees.size
+    end 
+
+    def close 
+        @employees.clear and @funding = 0 
+    end  
+
+    def acquire(startup_2) 
+        @funding += startup_2.funding 
+        startup_2.salaries.each do |title, salary| 
+            if !@salaries.has_key?(title) 
+                @salaries[title] = salary 
+            end 
+        end  
+        @employees += startup_2.employees 
+        startup_2.close
     end 
 end
