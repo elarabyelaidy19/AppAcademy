a559f19e15b201fb61f842a8027a23de779fe71c
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-27 14:51:05 +0200

    master mind

--- new/SWE/oop/mastermind_project/lib/code.rb
+++ new/SWE/oop/mastermind_project/lib/code.rb
@@ -1,9 +1,64 @@
 class Code
+  attr_reader :pegs
+  
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
     "B" => :blue,
     "Y" => :yellow
   }
+
+
+
+  def self.valid_pegs?(chars) 
+    chars.all? { |c| POSSIBLE_PEGS.has_key?(c.upcase) } 
+  end   
+
+  def self.random(length)
+    arr_pegs = []
+    length.times { arr_pegs << POSSIBLE_PEGS.keys.sample }
+    Code.new(arr_pegs)
+  end 
+
+
+  def initialize(pegs) 
+    if Code.valid_pegs?(pegs) 
+      @pegs = pegs.map(&:upcase) 
+    else 
+      raise "does not contain valid pages" 
+    end 
+  end 
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(''))
+  end 
+
+  def [](index) 
+    @pegs[index]
+  end 
+
+  def length
+    @pegs.size
+  end 
+
+  def num_exact_matches(code) 
+    count = 0 
+    (0...code.length).each do |i| 
+      count += 1 if code[i] == self[i] 
+    end
+    count 
+  end 
+
+  def num_near_matches(code) 
+    count = 0 
+    (0...code.length).each do |i| 
+      count += 1 if self.pegs.include?(code[i]) && code[i] != self[i]  
+    end 
+    count
+  end 
+
+  def ==(code) 
+    self.pegs == code.pegs
+  end 
 
 end
--- new/SWE/oop/mastermind_project/lib/mastermind.rb
+++ new/SWE/oop/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,5 @@
 require_relative "code"
 
 class Mastermind
-
+    
 end
