dc89668a1bc0b219bb4a386b052c980ecd841a90
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-29 14:40:32 +0200

    board of battelship

--- new/SWE/oop/battleship_project/lib/board.rb
+++ new/SWE/oop/battleship_project/lib/board.rb
@@ -1,3 +1,78 @@
 class Board
-  
+    attr_reader :size, :grid
+    def initialize(n) 
+        @grid = Array.new(n) {Array.new(n, :N)} 
+        @size = n**2
+    end 
+
+    def [](a) 
+        row = a[0] 
+        col = a[1]
+        @grid[row][col]
+    end 
+
+    def []=(pos, val) 
+        row = pos[0] 
+        col = pos[1]  
+        @grid[row][col] = val
+    end 
+
+    def num_ships 
+        # @grid.flatten.count(:S) 
+        count = 0 
+        @grid.each do |row| 
+            row.each_index do |col| 
+                count += 1 if row[col] == :S 
+            end 
+        end 
+        count 
+    end 
+
+    def attack(pos) 
+        if self[pos] == :S 
+            self[pos] = :H 
+            p 'you sunk my battelship!'  
+            true
+        else 
+            self[pos] = :X 
+            false
+        end 
+    end 
+
+    def place_random_ships
+        num = @size * 0.25
+        while self.num_ships < num 
+            row = rand(0...@grid.length) 
+            col = rand(0...@grid.length) 
+            pos = [row, col] 
+            self[pos] = :S 
+        end 
+        
+    end 
+
+    def hidden_ships_grid
+        @grid.map do |row| 
+            row.map do |ele| 
+                if ele == :S 
+                    ele = :N 
+                else 
+                    ele 
+                end 
+            end 
+        end 
+    end 
+
+    def self.print_grid(grid) 
+        grid.each do |row| 
+           puts row.join(' ')
+        end 
+    end 
+    def cheat
+        Board.print_grid(@grid) 
+    end 
+
+    def print
+        Board.print_grid(hidden_ships_grid)
+    end 
+
 end
--- new/SWE/oop/battleship_project/lib/player.rb
+++ new/SWE/oop/battleship_project/lib/player.rb
@@ -1,3 +1,6 @@
-class Player
+class Player    
+    def
+        
+    end 
 
 end
