d4396853bd66d5c7b2daa914b518890bc2feca41
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-29 15:09:21 +0200

    end of battleship

--- new/SWE/oop/battleship_project/lib/player.rb
+++ new/SWE/oop/battleship_project/lib/player.rb
@@ -1,6 +1,8 @@
 class Player    
-    def
-        
+    def get_move 
+        puts 'enter a position with coordinates separated with a space like `4 7'''
+        input = gets.chomp.split(' ') 
+        input.map(&:to_i)
     end 
 
 end
