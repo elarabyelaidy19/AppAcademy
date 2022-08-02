d1a0c2d7e5caa8d53992bf8fe8bf290cbab6a36d
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-08-02 23:37:41 +0200

    recap 3

--- new/SWE/additional_projects/recap_exercise_3.rb
+++ new/SWE/additional_projects/recap_exercise_3.rb
@@ -51,38 +51,77 @@ end
 
 
 def bi_prime?(num)
-
+    primes = prime_factors(num) 
+    factors = [] 
+    primes.any? do |pri| 
+        b = num / pri * 1.0
+        primes.include?(b)  
+    end     
 end
 
 def prime?(num)
- 
+    (2...num).each do |n| 
+        return false if num % n == 0 
+    end 
+    true
 end
 
 def prime_factors(num)
+    (2..num).select { |n| num % n == 0 && prime?(n) }
 end
+
+p prime_factors(14)
+p bi_prime?(24)
 
 
 def vigenere_cipher(message, keys)
-   
+    alph = ("a".."z").to_a 
+    encrypted = "" 
+    message.each_char.with_index do |c, i| 
+        idx = alph.index(c) 
+        key = keys[i % keys.length]
+        new_idx = idx +  key 
+        encrypted += alph[new_idx%26] 
+    end
+    encrypted
 end
+
+p vigenere_cipher("toerrishuman", [1,2])
 
 
 def vowel_rotate(str)
-   
+    vowels = "aioue" 
+    new_str = str[0..-1] 
+    vowel_idxs = (0...str.length).select { |i| vowels.include?(str[i]) }  
+    new_idxs = vowel_idxs.rotate(-1) 
+    vowel_idxs.each_with_index do |vowel_idx, i| 
+        new_vowel = str[new_idxs[i]] 
+        new_str[vowel_idx] = new_vowel 
+    end 
+    new_str
 end
 
+p vowel_rotate("oranges")
 
 # Proc Problems
 
 class String
     def select(&prc)
-       
+        res = ""
+        self.each_char do |c| 
+            res += c if prc.call(c) 
+        end 
+        res
     end
 
     def map!(&prc)
-        
+        self.each_char.with_index do |c, i| 
+            self[i] = c if prc.call(c, i) 
+        end 
     end
 end
+
+p "app academy".select { |c| !"aioue".include?(c) }
 
 # Recursion Problems
 
@@ -92,19 +131,18 @@ def multiply(a, b)
     return a + mul
 end
 
-p multiply(3 , 2) 
+#p multiply(3 , 2) 
 
 def lucas_sequence(n)
     return [] if n == 0
     return [2] if n == 1 
     return [2, 1] if n == 2 
-    seq = lucas_sequence(n-1) 
-    p seq
+    seq = lucas_sequence(n-1)
     seq << seq[-1] + seq[-2]
     seq
 end
 
-p lucas_sequence(4)
+#p lucas_sequence(4)
 
 
 def prime_factorization(num)
