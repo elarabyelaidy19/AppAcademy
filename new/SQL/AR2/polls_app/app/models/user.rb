1f16822a42b45aca20c4bf1c60997edd13c5e966
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-09-20 17:44:21 +0200

    polls responses

--- new/SQL/AR2/polls_app/app/models/user.rb
+++ new/SQL/AR2/polls_app/app/models/user.rb
@@ -9,5 +9,51 @@ class User < ApplicationRecord
     has_many :responses
 
 
-    
+    def completed_polls 
+        Poll.find_by_sql(<<-SQL) 
+            select poll.* 
+            from poll 
+            join  question on polls.id = questions.poll_id 
+            join answer_choices on question.id = answer_choices.question_id 
+            left outer join ( 
+                select * 
+                from responses 
+                where user_id = #{self.id} 
+            ) as responses on answer_choices.id = answer_choice_id 
+            group by polls.id 
+            having count(distinct questions.id) = count(responses.id)
+        SQL
+    end 
+
+
+
+    def completed_polls_AR 
+        polls_with_complete_count
+            .having('count(distinct questions.id) = count(responses.id)')
+    end 
+
+    def uncompleted_polls 
+        polls_with_complete_count
+            .having('count(distinct questions.id) = count(responses.id)') 
+            .having('count(responses.id) > 0')
+
+    end 
+
+
+    private 
+
+    def polls_with_complete_count 
+        joins_sql = (<<-SQL) 
+            left outer join ( 
+                select * 
+                from responses 
+                where user_id = #{self.id} 
+            )
+        SQL
+
+        Poll.joins(questions: :answer_choices) 
+            .joins(joins_sql) 
+            .group('polls.id')
+    end 
+
 end
